70562c7ccbac3174cab2e1eada2ebc82c03fb371
Yury Rivera <yrivera@applaudostudios.com>
2022-12-29 20:56:30 -0600

    Agregando conteo de vidas

--- adivina/elnumero.js
+++ adivina/elnumero.js
@@ -1,7 +1,54 @@
+let inputTag = document.getElementById('input-number');
+let formTag = document.getElementById('FormItem');
+let ulTag = document.getElementById('attemp-history');
+let divTag = document.getElementById('message-text');
+let h3Tag = document.getElementById('life-counter');
+
+let secretNumber = 4;
+let vidas = 8;
+updateLifeCounter(vidas);
+
+function updateLifeCounter(lifeCount){
+	let text = "Vidas: ";
+	let counter = 0;
+	while(counter < lifeCount) {
+		text = text + "♥";
+		counter++;
+	}
+	text = text + ' ' + lifeCount; 
+	h3Tag.textContent = text;
+}
+
+formTag.addEventListener('submit', (evt)=> {
+	evt.preventDefault();
+	let currentValue = inputTag.value;
+	// validar si hay vidas
+	if (secretNumber == currentValue){
+		// mostrar aviso
+		alert('ganaste');
+	} else {
+		//valio verga
+		vidas--;
+		updateLifeCounter(vidas);
+		if(vidas <= 0){
+			// se acabo
+			alert('perdiste');
+		}
+		alert('Intentalo denuevo');
+
+		if (currentValue > num) {
+			alert("el número que pensé es menor");
+		}
+
+		if (currentValue < num) {
+			alert("el número que pensé es mayor");
+		}	
+	}
+	
+});
 
 
 function iniciar() {
-	let vidas = 10;
 	let num = Math.floor((Math.random() * 100) + 1);
 	while (vidas > 0) {
 		let numero = prompt("Te quedan " + vidas +
@@ -10,8 +57,7 @@ function iniciar() {
 			alert("¡Felicidades, ganaste! :D ");
 
 		}
-		if (numero > num) {
-			alert("el número que pensé es menor");
+		
 		}
 		if (numero < num) {
 			alert("el número que pensé es mayor");
@@ -19,8 +65,4 @@ function iniciar() {
 		vidas--;
 	}
 
-	if (vidas == 0) {
-		alert("Lo siento, perdiste :( ");
-	}
 //Alan gei
-} 
